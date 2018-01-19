419d2b22b7ad0cdce7481751485cae3dcc8ca28a
Dario Molina <darioporta88@gmail.com>
2018-01-19 10:44:25 +0000

    Added slideshow to home page and changed the background color.

--- php/home.php
+++ php/home.php
@@ -6,9 +6,58 @@
     </head>
     <body>
         <div id="cointer">
+            
+            <!-- Image div -->
             <div id="imageLogo">
-                <img src="../images/barberLogo.jpg" alt="Logo" height="300" width="500"></img>
+                <img src="../images/barberLogo.jpg" alt="Logo" height="300" width="600">
             </div>
+            
+            <!-- Slideshow -->
+            <div id="categories">
+                <ul>
+                    <li><a href="./home.php">Home</a></li>
+                    <li><a href="./shop.php">Shop</a></li>
+                    <li><a href="./shop.php">About</a></li>
+                    <li><a href="./shop.php">Contact</a></li>
+                </ul>
+            </div>
+            
+            <div class="slideshow-container">
+                    <div class="mySlides fade">
+                      <img class = "slideImage" src="../images/pic4.jpg" style="width:100%" alt="image not found">
+                    </div>
+                    
+                    <div class="mySlides fade">
+                      <img class = "slideImage" src="../images/pic2.jpg" style="width:100%" alt="image not found">
+                    </div>
+                    
+                    <div class="mySlides fade">
+                      <img class = "slideImage" src="../images/pic3.jpg" style="width:100%" alt="image not found">
+                    </div>
+            </div>
+            
+        
+        
+        <!-- End of div container -->
         </div>
+        
+        <script>
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    
+    if (slideIndex > slides.length) {slideIndex = 1;}    
+    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
+</script>
     </body>
 </html>
