7a664cd6a745ffaeedd8047199cdb71eae4c3150
Dario Molina <darioporta88@gmail.com>
2018-01-19 09:00:08 +0000

    Initial commit.

---
+++ php/home.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Gomez The Barber</title>
+        <link rel="stylesheet" href="../css/style.css">
+        
+    </head>
+    <body>
+        <div id="cointer">
+            <div id="imageLogo">
+                <img src="../images/barberLogo.jpg" alt="Logo" height="300" width="500"></img>
+            </div>
+        </div>
+    </body>
+</html>
