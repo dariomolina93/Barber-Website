19fcd9f4feb8825079b6ac67bd7017f5bbb70ec6
Dario Molina <darioporta88@gmail.com>
2018-01-20 04:55:47 +0000

    Added button style to arrivals.

--- php/home.php
+++ php/home.php
@@ -51,10 +51,9 @@
                         </div>
                         
                         <div class = "displayVertical">
-                            <div>Red Fade Bandana</div>
+                            <div class = "nameOfProduct">Red Fade Bandana</div>
                             <button>$35</button>
                         </div>
-                        
                     </div>
                     
                     <!-- inner product container -->
@@ -64,37 +63,58 @@
                         </div>
                         
                         <div class = "displayVertical">
-                            <div>Red Fade Bandana</div>
+                            <div class = "nameOfProduct">Red Fade Bandana</div>
                             <button>$35</button>
                         </div>
                     </div>
                     
+                    <!-- inner product container -->
+                    <div class = "innerContainer">
+                        <div>
+                            <img class="productImage" src="https://square-production.s3.amazonaws.com/files/3e199fc39a96aaf2193d8c77e64fc450508feb6f/original.jpeg">
+                        </div>
+                        
+                        <div class = "displayVertical">
+                            <div class = "nameOfProduct">Red Fade Bandana</div>
+                            <button>$35</button>
+                        </div>
+                    </div>
+                    
+                    <!-- inner product container -->
+                    <div class = "innerContainer">
+                        <div>
+                            <img class="productImage" src="https://square-production.s3.amazonaws.com/files/3e199fc39a96aaf2193d8c77e64fc450508feb6f/original.jpeg">
+                        </div>
+                        
+                        <div class = "displayVertical">
+                            <div class = "nameOfProduct">Red Fade Bandana</div>
+                            <button>$35</button>
+                        </div>
+                    </div>
                     
                 </div>
             </div>
             
-        
-        
         <!-- End of div container -->
         </div>
         
         <script>
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    
-    if (slideIndex > slides.length) {slideIndex = 1;}    
-    
-    slides[slideIndex-1].style.display = "block";  
-    setTimeout(showSlides, 4000); // Change image every 4 seconds
-}
-</script>
+            var slideIndex = 0;
+            showSlides();
+            
+            function showSlides() {
+                var i;
+                var slides = document.getElementsByClassName("mySlides");
+                for (i = 0; i < slides.length; i++) {
+                   slides[i].style.display = "none";  
+                }
+                slideIndex++;
+                
+                if (slideIndex > slides.length) {slideIndex = 1;}    
+                
+                slides[slideIndex-1].style.display = "block";  
+                setTimeout(showSlides, 4000); // Change image every 4 seconds
+            }
+        </script>
     </body>
 </html>
