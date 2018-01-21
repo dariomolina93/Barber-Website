48ecb28ccc98093dd9970c167da3fc8f04ce4826
Dario Molina <darioporta88@gmail.com>
2018-01-21 00:03:26 +0000

    Added composer.json for heroku and currently working on modal.

--- php/home.php
+++ php/home.php
@@ -66,11 +66,41 @@
                                 
                                 <div class = \"displayVertical\">
                                     <div class = \"nameOfProduct\">". $arrivals[$i]["name"]."</div>
-                                    <button>".$arrivals[$i]["price"]."</button>
+                                    <button id='button".$i."' onclick=\"displayModal('button".$i."')\">".$arrivals[$i]["price"]."</button>
                                 </div>
                               
                               <!-- end of inner product container -->
                             </div>";
+                            
+                            echo "
+                            
+                            
+                            <!-- The Modal -->
+                            <div id=\"modal".$i."\" class=\"modal\">
+                            
+                              <!-- Modal content -->
+                              <!-- Main container -->
+                              <div style=\"display: flex;\" class=\"modal-content\">
+                                    
+                                    <!-- Left side (description) -->
+                                    <div>
+                                        <h2>".$arrivals[$i]["name"]."</h2>
+                                    
+                                    </div>
+                                    
+                                    <!-- Image -->
+                                    <div>
+                                        <img class=\"productImage\" src=\"".$arrivals[$i]["picture"]."\">
+                                    
+                                    </div>
+    
+                                   
+                              <!-- End of div modal container -->
+                              </div>
+                        
+                            
+                            </div>";
+                            
                         }
                         
                     ?>
@@ -82,6 +112,108 @@
         </div>
         
         <script src="../js/javascript.js" type="text/javascript"></script>
+        
+        <script type="text/javascript">
+        
+        document.addEventListener("DOMContentLoaded", function(event) { 
+ 
+                        /*
+                // Get the modal
+                var modal = document.getElementById(modals);
+                        
+                // Get the button that opens the modal
+                var btn = document.getElementById(buttons);
+                */
+                
+                var modals = [];
+                var buttons = [];
+                
+                var i;
+                
+                for(i = 0; i< 4; i++)
+                {
+                    modals[i] = document.getElementById("modal"+i)
+                    buttons[i] = document.getElementById("button"+i)
+                    
+                    console.log("modals[i]=" +modals[i])
+                    console.log("buttons[i]=" +buttons[i])
+                    
+                }
+                
+                console.log("after first for loop")
+                console.log("i= " + i)
+                console.log("modals[i]=" +modals[i])
+                console.log("modals[i]=" +modals[3])
+                console.log("buttons[i]=" +buttons[3])
+    
+                        
+                
+                /*
+                // Get the <span> element that closes the modal
+                var span = document.getElementsByClassName("close")[0];
+                
+                span.onclick = function() 
+                {
+                modal.style.display = "none";
+                }
+                */ 
+                
+                
+                for(i= 0; i< 4; i++)
+                {
+                    console.log("Inside second for loop")   
+                    console.log("i= " + i)
+                    console.log("modals[i]=" +modals[i])
+                    
+                    buttons[i].onclick = function()
+                    {
+                        console.log("modals[i]=" +modals[i])
+                        console.log("buttons[i]=" +buttons[i])
+                        console.log("i= " + i)
+                        modals[i].style.display = "block";
+                    }
+                }
+                
+                
+                 /*       
+                // When the user clicks the button, open the modal 
+                btn.onclick = function()
+                {
+                modal.style.display = "block";
+                }
+                */
+                        
+                // When the user clicks anywhere outside of the modal, close it
+                window.onclick = function(event)
+                {
+                    for(i = 0; i < 4; i++)
+                        {
+                            if (event.target == modals[i])
+                            {
+                                modals[i].style.display = "none";
+                            }
+                        }
+                }
+            
+        });
+        
+        /*
+                        
+                function displayModal(buttonId)
+                {
+                    var i;
+                    
+                    for(i = 0; i < 4; i++)
+                    {
+                        if(buttons[i].id === buttonId)
+                        {
+                            modals[i].style.display = "block";
+                        }
+                    }
+                }
+        */
+            
+</script>
     </body>
 </html>
 
